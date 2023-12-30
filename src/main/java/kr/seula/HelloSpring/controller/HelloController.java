7164415958a9829d10d6fb983892723e9d66a22f
rheeseula <rheeseula@dgsw.hs.kr>
2023-12-30 18:59:08 +0900

    🚰 | MVC와 템플릿 엔진

--- src/main/java/kr/seula/HelloSpring/controller/HelloController.java
+++ src/main/java/kr/seula/HelloSpring/controller/HelloController.java
@@ -3,9 +3,11 @@ package kr.seula.HelloSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+    // 라우트를 GET 방식으로 매핑
     @GetMapping("hello")
     // Model은 인터페이스이지만 IoC여서 스프링이 객체를 넣어줌
     public String hello(Model model) {
@@ -13,5 +15,12 @@ public class HelloController {
 
         // HelloController -> viewResolver
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    // 파라미터로 name을 받아서 model 객체에 name 이름으로 추가
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
