380bcbf3f07c3cfc886bca226e90ac5a7cbf0163
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-03 15:31:26 +0900

    ⛄ | 회원 웹 기능 - 조회

--- src/main/java/kr/seula/HelloSpring/controller/MemberController.java
+++ src/main/java/kr/seula/HelloSpring/controller/MemberController.java
@@ -4,8 +4,11 @@ import kr.seula.HelloSpring.domain.Member;
 import kr.seula.HelloSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /*
     @Controller
@@ -83,5 +86,12 @@ public class MemberController {
             스프링에서 HTTP 리디렉션 응답을 함
          */
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
